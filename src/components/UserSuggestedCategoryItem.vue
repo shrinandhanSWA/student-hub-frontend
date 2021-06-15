67a362324f356ed7df10609e263320ab02773d3e
sp2419 <sp2419@ic.ac.uk>
2021-06-15 14:07:39 +0700

    UPDATE join button CSS suggested

--- src/components/UserSuggestedCategoryItem.vue
+++ src/components/UserSuggestedCategoryItem.vue
@@ -27,150 +27,126 @@
           {{ category.description }}
         </p>
       </div>
-      <i id="joinBlock"
+      <div class="actions">
+        <i id="joinBlock"
            v-if="isLoggedIn"
            class="join-button"
            @click.prevent.stop="showConfirmDialog= true"
         >Join</i>
+      </div>
     </router-link>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+  import { mapActions } from 'vuex'
 
-export default {
-  props: {
-    category: {
-      type: Object,
-      default: () => ({})
-    }
-  },
-
-  data () {
-    return {
-      showConfirmDialog: false,
-      newPosts: false,
-      missingPosts: 0
-    }
-  },
-
-  methods: {
-    ...mapActions(['joinSuggestedCategory'])
-  },
-
-  async mounted () {
-      try {
-        const {out, diff} = await this.checkNewPosts({
-        categorySlug: this.category.slug,
-          data: {
-            email: this.currentUser.email
-          }
-        })
-        this.newPosts = out
-        this.missingPosts = diff
-       
-      } catch (err) {
-        this.loading = false
-        this.error = true
+  export default {
+    props: {
+      category: {
+        type: Object,
+        default: () => ({})
       }
-    }
-}
+    },
+
+    data () {
+      return {
+        join: false,
+        showConfirmDialog: false,
+      }
+    },
+
+    methods: {
+    ...mapActions(['joinSuggestedCategory'])
+  }
+  }
 </script>
 
 <style lang="stylus" scoped>
-.category-item-link
-  background: #FFF
-  padding: 15px
-  cursor: pointer
-  transition: 0.2s background ease-out
-  border-left: 5px solid
-  box-shadow: 0 1px 5px 0 hsla(0, 0, 0, 0.2)
-  border-radius: 4px
-  display: flex
-  text-decoration: none
-  justify-content: space-between
+  .category-item-link
+    background: #FFF
+    padding: 15px
+    cursor: pointer
+    transition: 0.2s background ease-out
+    border-left: 5px solid
+    box-shadow: 0 1px 5px 0 hsla(0, 0, 0, 0.2)
+    border-radius: 4px
+    display: flex
+    text-decoration: none
+    justify-content: space-between
 
-.circle {
-  width: 30px
-  height: 30px
-  line-height: 30px
-  border-radius: 50%
-  font-size: 15px
-  color: black
-  text-align: center
-  background: #00cc00
-}
+  .category-item:hover
+    background: #F9F9F9
 
-.link {
-  text-decoration: none
-}
+  .title
+    font-size: 20px
+    margin: 0
+    font-weight 400
+    color: #000
 
-.category-item:hover
-  background: #F9F9F9
+  .description
+    color: #999
+    margin: 10px 0 0
 
-.title
-  font-size: 20px
-  margin: 0
-  font-weight 400
-  color: #000
+  .actions
+    display: flex
+    justify-content: flex-end
+    align-items: center
 
-.description
-  color: #999
-  margin: 10px 0 0
+  .action-button
+    margin-left: 15px
+    color: $primaryColor
+    cursor: pointer
 
-.actions
-  display: flex
-  justify-content: flex-end
-  align-items: center
-
-.action-button
-  margin-left: 15px
-  color: $primaryColor
-  cursor: pointer
-
-.action-button:hover
-  color: lighten($primaryColor, 20%)
-
-.join-button {
-  display: inline-block;
-  padding: .3rem 1rem;
-  margin-left: 1.3em;
-  font-style : normal;
-  border-radius: 10rem;
-  color: #fff;
-  font-size: 1rem;
-  transition: all .3s;
-  position: relative;
-  overflow: hidden;
-  z-index: 1;
-
-  &:after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: $primaryColor;
+  .join-button {
+    display: inline-block;
+    padding: .3rem 1rem;
+    margin-left: 1.3em;
+    font-style: normal;
     border-radius: 10rem;
-    z-index: -2;
-  }
-  &:before {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 0;
-    height: 100%;
-    background-color: darken($primaryColor, 15%);
-    transition: all .3s;
-    border-radius: 10rem;
-    z-index: -1;
-  }
-  &:hover {
     color: #fff;
+    font-size: 1rem;
+    transition: all .3s;
+    position: relative;
+    overflow: hidden;
+    z-index: 1;
+
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: $primaryColor;
+      border-radius: 10rem;
+      z-index: -2;
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 0;
+      height: 100%;
+      background-color: darken($primaryColor, 15%);
+      transition: all .3s;
+      border-radius: 10rem;
+      z-index: -1;
+    }
+
+    &:hover {
+      color: #fff;
+
+      &:before {
+        width: 100%;
+      }
+    }
   }
-}
+
+
+  .action-button:hover
+    color: lighten($primaryColor, 20%)
 
 </style>
